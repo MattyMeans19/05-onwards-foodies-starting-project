a2065b57d20ead7be16469a10e1b92250cc87358
Matt <mattymeans@gmail.com>
2025-10-17 00:47:56 -0600

    Massive Update

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,33 @@
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { getMeal } from "@/lib/meals";
+
 export default function MealDetails({params}){
-    return(
+    const meal = getMeal(params.mealSlug);
+
+    if(!meal){
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+    return <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+                <p className={classes.summary}>{meal.summary}</p>
+            </div>
+        </header>
         <main>
-            <h1>Meal Details</h1>
-            <h2>{params.mealSlug}</h2>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{
+                __html: meal.instructions,
+            }}></p>
         </main>
-    )
+    </>
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,29 @@
-export default function Meals(){
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meal-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals(){
+    const meals = await getMeals();
+
+    return <MealsGrid meals = {meals} />
+}
+
+export default function MealsPage(){
+
     return(
-        <main>
-            <h1>Meals</h1>
+    <>
+        <header className={classes.header}>
+            <h1>Delicious Meals, created <span className={classes.highlight}>by you</span></h1>
+            <p>Choose your favorite recipe and coot it yourself. It is easy and fun!</p>
+            <p className={classes.cta}> <Link href="/meals/share">Share your favorite recipe!</Link></p>
+        </header>
+        <main className={classes.main}>
+            <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+               <Meals /> 
+            </Suspense>    
         </main>
+    </>
     );
 }
